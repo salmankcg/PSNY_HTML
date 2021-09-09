315e5beef21060518949ec0af3077b340728eda1
Nazifa Rashid <nazifarashid1822@gmail.com>
2021-09-09 16:06:26 +0600

    Homepage css 90%

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,5 @@
 let mix = require('laravel-mix');
 
 mix.js('src/js/app.js', 'dist').setPublicPath('dist')
-    .sass('src/scss/styles.scss', 'dist').setPublicPath('dist');
+    .sass('src/scss/styles.scss', 'dist').setPublicPath('dist')
+    .browserSync('my-domain.test');
