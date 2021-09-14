6555ee75874e106b1c7462df915a11d67aab4e21
Nazifa Rashid <nazifarashid1822@gmail.com>
2021-09-15 01:21:47 +0600

    Did changes as discussed

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
 import 'bootstrap';
+
+const header = document.querySelector('.header');
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    header.classList.add('header-sticky');
+  } 
+  else {
+    header.classList.remove('header-sticky');
+  }
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,6 @@
 
 let mix = require('laravel-mix');
 
-mix.js('src/js/app.js', 'dist').setPublicPath('dist')
-    .sass('src/scss/styles.scss', 'dist').setPublicPath('dist')
+mix.js('src/js/app.js', 'assets/js').setPublicPath('assets')
+    .sass('src/scss/styles.scss', 'assets/css').setPublicPath('assets')
     .browserSync('my-domain.test');
